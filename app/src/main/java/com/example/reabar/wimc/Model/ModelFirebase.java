7f7e00040d8849557aeabfdeb35fc5acf5d31bec
Rea Bar <Rea.Bar@gmail.com>
2016-06-27 13:53:55 +0300

    working with new firebase version

---
+++ app/src/main/java/com/example/reabar/wimc/Model/ModelFirebase.java
@@ -0,0 +1,11 @@
+package com.example.reabar.wimc.Model;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+/**
+ * Created by reabar on 27.6.2016.
+ */
+public class ModelFirebase {
+    FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+
+}
