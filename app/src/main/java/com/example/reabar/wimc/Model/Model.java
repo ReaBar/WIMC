400fe7c62f211168c3bda49649dc60d1e1a60131
Admin <admin@Macintosh-4.local>
2016-07-30 20:18:28 +0300

    start working on cloudinary

--- app/src/main/java/com/example/reabar/wimc/Model/Model.java
+++ app/src/main/java/com/example/reabar/wimc/Model/Model.java
@@ -1,19 +1,41 @@
 package com.example.reabar.wimc.Model;
 
+import com.example.reabar.wimc.FilesManagerHelper;
+import com.example.reabar.wimc.MyApplication;
 import com.google.firebase.auth.FirebaseAuth;
-
 import java.util.LinkedList;
 import java.util.List;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.util.Log;
+
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 /**
  * Created by reabar on 28/06/2016.
  */
 public class Model {
     private static Model ourInstance = new Model();
-
+    private Bitmap image;
     private ModelFirebase modelFirebase;
+    private ModelCloudinary modelCloudinary;
     private ModelSql modelSql;
     private User currentUser;
+    private static boolean imageBool;
+    private FilesManagerHelper fileManager;
 
     public static Model getInstance() {
         return ourInstance;
@@ -22,6 +44,7 @@ public class Model {
     private Model() {
         modelFirebase = new ModelFirebase();
         modelSql = new ModelSql();
+        modelCloudinary = new ModelCloudinary(MyApplication.getAppContext());
     }
 
     public void setCurrentUser(User currentUser) {
@@ -113,4 +136,133 @@ public class Model {
         void success(boolean success);
         void failed(String message);
     }
+
+
+
+    public void loadImage(final String imageName, final LoadImageListener listener) {
+        AsyncTask<String,String,Bitmap> task = new AsyncTask<String, String, Bitmap >() {
+            @Override
+            protected Bitmap doInBackground(String... params) {
+                //first try to fin the image on the device
+                Bitmap bmp = fileManager.loadImageFromFile(imageName);
+
+                if (bmp == null) {
+                    bmp = modelCloudinary.loadImage(imageName);
+                    //save the image locally for next time
+                    if (bmp != null)
+                        fileManager.saveImageToFile(bmp,imageName);
+                }
+                return bmp;
+            }
+            @Override
+            protected void onPostExecute(Bitmap result) {
+                listener.onResult(result);
+            }
+        };
+        task.execute();
+    }
+
+
+    public interface LoadImageListener{
+        public void onResult(Bitmap imageBmp);
+    }
+  /*  public void SetLocalBitmap(Bitmap image)
+    {
+        this.image = image;
+        for(int i=0; i<data.size(); i++)
+            data.get(i).setImageProduct(image);
+
+        modelCloudinary.uploadImage(data.get(0).getImageProductLink(), data.get(0).getImageProduct());
+    }
+*/
+
+/*
+    public void saveImage(final Bitmap imageBitmap, final String imageName) {
+        saveImageToFile(imageBitmap,imageName); // synchronously save image locally
+        Thread d = new Thread(new Runnable() {  // asynchronously save image to parse
+            @Override
+            public void run() {
+                modelCloudinary.saveImage(imageBitmap,imageName);
+            }
+        });
+        d.start();
+    }
+
+    public interface LoadImageListener{
+        public void onResult(Bitmap imageBmp);
+    }
+
+    public void loadImage(final String imageName, final LoadImageListener listener) {
+        AsyncTask<String,String,Bitmap> task = new AsyncTask<String, String, Bitmap >() {
+            @Override
+            protected Bitmap doInBackground(String... params) {
+                Bitmap bmp = loadImageFromFile(imageName);              //first try to fin the image on the device
+                if (bmp == null) {                                      //if image not found - try downloading it from parse
+                    bmp = modelCloudinary.loadImage(imageName);
+                    if (bmp != null) saveImageToFile(bmp,imageName);    //save the image locally for next time
+                }
+                return bmp;
+            }
+
+            @Override
+            protected void onPostExecute(Bitmap result) {
+                listener.onResult(result);
+            }
+        };
+        task.execute();
+    }
+
+    private void saveImageToFile(Bitmap imageBitmap, String imageFileName){
+        FileOutputStream fos;
+        OutputStream out = null;
+        try {
+            //File dir = context.getExternalFilesDir(null);
+            File dir = Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_PICTURES);
+            if (!dir.exists()) {
+                dir.mkdir();
+            }
+            File imageFile = new File(dir,imageFileName);
+            imageFile.createNewFile();
+
+            out = new FileOutputStream(imageFile);
+            imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+            out.close();
+
+            //add the picture to the gallery so we dont need to manage the cache size
+            Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+            Uri contentUri = Uri.fromFile(imageFile);
+            mediaScanIntent.setData(contentUri);
+            context.sendBroadcast(mediaScanIntent);
+            Log.d("tag","add image to cache: " + imageFileName);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Bitmap loadImageFromFile(String imageFileName){
+        String str = null;
+        Bitmap bitmap = null;
+        try {
+            File dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+            File imageFile = new File(dir,imageFileName);
+
+            //File dir = context.getExternalFilesDir(null);
+            InputStream inputStream = new FileInputStream(imageFile);
+            bitmap = BitmapFactory.decodeStream(inputStream);
+            Log.d("tag","got image from cache: " + imageFileName);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bitmap;
+    }
+
+*/
+
 }
---
+++ app/src/main/java/com/example/reabar/wimc/Model/ModelCloudinary.java
@@ -0,0 +1,81 @@
+package com.example.reabar.wimc.Model;
+
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.ByteBuffer;
+import java.util.Map;
+
+/**
+ * Created by admin on 01/07/16.
+ */
+public class ModelCloudinary {
+    Cloudinary cloudinary;
+
+    public ModelCloudinary(Context context){
+        cloudinary = new Cloudinary("cloudinary://994665469628467:GR0yXpujp0T4I4Ll83QL05Zy6R4@wimc");
+    }
+
+
+    public void uploadImage(final String imageName,final Bitmap image)
+    {
+        if(image == null)
+            return;
+
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                    image.compress(Bitmap.CompressFormat.JPEG, 100, bos);
+                    byte[] bitmapdata = bos.toByteArray();
+                    ByteArrayInputStream bs = new ByteArrayInputStream(bitmapdata);
+                    //String name = imageName.substring(0,imageName.lastIndexOf("."));
+                    Map res = null;
+                    try {
+                        res = cloudinary.uploader().upload(bs, ObjectUtils.asMap("public_id", imageName));
+                        Log.d("TAG", "save image to url" + res.get("url"));
+                    } catch (IOException e) {
+                        Log.d("TAG", e.getMessage());
+                        e.printStackTrace();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Log.d("TAG", ex.getMessage());
+                }
+            }
+        });
+        t.start();
+    }
+
+    public Bitmap loadImage(String imageName) {
+        URL url = null;
+        try {
+            url = new URL(cloudinary.url().generate(imageName));
+            Log.d("TAG", "load image from url" + url);
+            Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+            return bmp;
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Log.d("TAG", "url" + url);
+
+        return null;
+    }
+}
+
